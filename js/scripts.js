863bc11e96f0e2e83f628bc2a008d22937450477
kc <kelcrisp007@icloud.com>
2019-06-07 12:47:33 -0700

    updated colors for text and set a new background image

--- js/scripts.js
+++ js/scripts.js
@@ -3,47 +3,47 @@ $(document).ready(function() {
     event.preventDefault();
 
 debugger;
-    var number1 = ($("input#style2").val());
-    var number2 = ($("input#money2").val());
-    var number3 = ($("input#life2").val());
-    var number4 = ($("input#beer2").val());
-    var number5 = ($("input#darts2").val());
+    var number1 = ($("input#littlemoney2").val());
+    var number2 = ($("input#bitmoremoney2").val());
+    var number3 = ($("input#decentmoney2").val());
+    var number4 = ($("input#lotsofmoney2").val());
+    var number5 = ($("input#richaf2").val());
 
 
     if(number1 === "yes"){
-      $("#style1").show();
-      $("#money1").hide();
-      $("#life1").hide();
-      $("#beer1").hide();
-      $("#darts1").hide();
+      $("#littlemoney1").show();
+      $("#bitmoremoney1").hide();
+      $("#decentmoney1").hide();
+      $("#lotsofmoney1").hide();
+      $("#richaf1").hide();
 
     }else if(number1 ==="no" && number2 === "yes"){
-      $("#style1").hide();
-      $("#money1").show();
-      $("#life1").hide();
-      $("#beer1").hide();
-      $("#darts1").hide();
+      $("#littlemoney1").hide();
+      $("#bitmoremoney1").show();
+      $("#decentmoney1").hide();
+      $("#lotsofmoney1").hide();
+      $("#richaf1").hide();
 
     }else if(number1 && number2 ==="no" && number3 === "yes"){
-      $("#style1").hide();
-      $("#money1").hide();
-      $("#life1").show();
-      $("#beer1").hide();
-      $("#darts1").hide();
+      $("#littlemoney1").hide();
+      $("#bitmoremoney1").hide();
+      $("#decentmoney1").show();
+      $("#lotsofmoney1").hide();
+      $("#richaf1").hide();
 
     }else if(number1,number2,number3 === "no" && number4 === "yes"){
-      $("#style1").hide();
-      $("#money1").hide();
-      $("#life1").hide();
-      $("#beer1").show();
-      $("#darts1").hide();
+      $("#littlemoney1").hide();
+      $("#bitmoremoney1").hide();
+      $("#decentmoney1").hide();
+      $("#lotsofmoney1").show();
+      $("#richaf1").hide();
 
-    }else if(number1,number2,number3,number4 === "no" && number5 === "yes"){
-      $("#style1").hide();
-      $("#money1").hide();
-      $("#life1").hide();
-      $("#beer1").hide();
-      $("#darts1").show();
+    }else if(number1 && number5 === "yes"){
+      $("#littlemoney1").hide();
+      $("#bitmoremoney1").hide();
+      $("#decentmoney1").hide();
+      $("#lotsofmoney1").hide();
+      $("#richaf1").show();
 
 
 }

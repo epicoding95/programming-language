561a21bdd5e092fd4fe38bdb0a8a20fd4cfaa26f
kc <kelcrisp007@icloud.com>
2019-06-07 11:12:29 -0700

    all questions are functionable

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,44 @@
 $(document).ready(function() {
-  $("form#ready").submit(function(event) {
+  $("#ready").submit(function(event) {
     event.preventDefault();
+
 debugger;
+    var number1 = ($("input#style2").val());
+    var number2 = ($("input#money2").val());
+    var number3 = ($("input#life2").val());
+    var number4 = ($("input#beer2").val());
+    var number5 = ($("input#darts2").val());
 
-    var number1 = ($("#style1").val());
-    var number2 = ($("#money1").val());
-    var number3 = ($("#life1").val());
-    var number4 = ($("#beer1").val());
-    var number5 = ($("#darts1").val());
 
-
-    if(style1 === "yes"){
+    if(number1 === "yes"){
       $("#style1").show();
       $("#money1").hide();
       $("#life1").hide();
       $("#beer1").hide();
       $("#darts1").hide();
 
-    }else if((total >= 11)&& (total <=30)){
+    }else if(number1 ==="no" && number2 === "yes"){
       $("#style1").hide();
       $("#money1").show();
       $("#life1").hide();
       $("#beer1").hide();
       $("#darts1").hide();
-    }else if((total >= 11)&& (total <=30)){
+
+    }else if(number1 && number2 ==="no" && number3 === "yes"){
       $("#style1").hide();
       $("#money1").hide();
       $("#life1").show();
       $("#beer1").hide();
       $("#darts1").hide();
-      }else if((total >= 11)&& (total <=30)){
+
+    }else if(number1,number2,number3 === "no" && number4 === "yes"){
       $("#style1").hide();
       $("#money1").hide();
       $("#life1").hide();
       $("#beer1").show();
       $("#darts1").hide();
-      }else if((total >= 11)&& (total <=30)){
+
+    }else if(number1,number2,number3,number4 === "no" && number5 === "yes"){
       $("#style1").hide();
       $("#money1").hide();
       $("#life1").hide();
@@ -43,7 +46,8 @@ debugger;
       $("#darts1").show();
 
 
-    }
+}
+
 
   });
   });
